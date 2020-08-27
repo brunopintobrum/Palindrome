5ca44f95e198f0f0c2ede9741fe4e1013b04bed4
Bruno Brum <brunobrum@gmail.com>
2020-08-27 19:48:09 -0400

    Programme pour verifier si un mot est un Palindrome

---
+++ Entities/Palindrome.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Palindrome
+{
+    class Palindrome
+    {
+        public string Word { get; set; }
+
+        public Palindrome()
+        {
+        }
+
+        public Palindrome(string word)
+        {
+            Word = word;
+        }
+
+        public static bool IsPalindrome(string word)
+        {
+            var length = word.Length;
+            for(int i = 0; i <= length / 2; i++)
+            {
+                if(word[i] != word[length - i - 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Por favor digite a palavra: ");
+            var word = Console.ReadLine().ToLower();
+
+            var result = Palindrome.IsPalindrome(word);
+
+            if (result == true)
+            {
+                Console.WriteLine("A palavra é um Palindrome!");
+            }
+            else
+            {
+                Console.WriteLine("Não é um Palindrome!");
+            }
+        }
+    }
+}
